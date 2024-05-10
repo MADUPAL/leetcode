cf44b6e2060cd8106dd6ee2a877410e75738a5db
MADUPAL <kevin488@naver.com>
2024-05-10 18:37:48 +0900

    recursive 206

--- python/206.py
+++ python/206.py
@@ -18,3 +18,16 @@ class Solution:
             curr = nextNode
         
         return prev
+
+        ###### recursive
+        # if not head:
+        #     return None
+        
+        # newHead = head
+
+        # if head.next:
+        #     newHead = self.reverseList(head.next)
+        #     head.next.next = head
+        # head.next = None
+
+        # return newHead
