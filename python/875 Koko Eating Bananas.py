111c7797dc866ecfd3f7c81993433b3ffe9f9718
MADUPAL <kevin488@naver.com>
2024-05-29 21:28:35 +0900

    875

---
+++ python/875 Koko Eating Bananas.py
@@ -0,0 +1,46 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        # def isCorrect(m):
+        #     t = 0
+        #     for i in piles:
+        #         Q, R = divmod(i, m)
+        #         Q = Q + 1 if R != 0 else Q
+        #         t += Q
+            
+        #     if t <= h:
+        #         return -1
+        #     elif t > h:
+        #         return 1
+
+        # def binarySearch(l, r):
+
+        #     rslt = max(piles)
+        #     while l <= r:
+        #         k = (l+r)//2
+        #         res = isCorrect(k)
+        #         if res > 0:
+        #             l = k + 1
+        #         else:
+        #             r = k - 1
+        #             rslt = min(rslt, k)
+        #     return rslt
+        
+        # return binarySearch(1, max(piles))
+
+        l, r = 1, max(piles)
+        rslt = r
+
+        while l <= r:
+            k = (l+r)//2
+            t = 0
+            for i in piles:
+                t += math.ceil(i/k)
+            
+            if t > h:
+                l = k + 1
+            else:
+                r = k - 1
+                rslt = min(rslt, k)
+        
+        return rslt
