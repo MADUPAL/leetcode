30a9dc64c725417780255780d53baafa71cf32ba
MADUPAL <kevin488@naver.com>
2024-06-08 21:46:44 +0900

    450 recap

--- python/450 Delete Node in a BST.py
+++ python/450 Delete Node in a BST.py
@@ -30,3 +30,31 @@ class Solution:
                 root.right = self.deleteNode(root.right, minNode.val)
         
         return root
+
+### recap
+# class Solution:
+#     def findMin(self, root):
+#         cur = root
+#         while cur and cur.left:
+#             cur = cur.left
+#         return cur
+
+#     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+#         if not root:
+#             return None
+        
+#         if key < root.val:
+#             root.left = self.deleteNode(root.left, key)
+#         elif key > root.val:
+#             root.right = self.deleteNode(root.right, key)
+#         else:
+#             if not root.left:
+#                 return root.right
+#             elif not root.right:
+#                 return root.left
+#             else:
+#                 minNode = self.findMin(root.right)
+#                 root.val = minNode.val
+#                 root.right = self.deleteNode(root.right, minNode.val)
+        
+#         return root
