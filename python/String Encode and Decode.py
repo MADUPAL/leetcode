c20c5d9613e22f41a384736b24f3dc2f65eb085a
MADUPAL <kevin488@naver.com>
2024-07-03 23:15:48 +0900

    String Encode and Decode

---
+++ python/String Encode and Decode.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        rslt = ''
+        for s in strs:
+            rslt += str(len(s))+'/'+s
+        
+        return rslt
+
+    def decode(self, s: str) -> List[str]:
+        # ans = []
+        # i, j = 0, 0
+        # numStr = ''
+        # num = 0
+        # while i < len(s) and j < len(s):
+        #     if s[i].isnumeric():
+        #         numStr += s[i]
+        #     elif s[i] == '/':
+        #         num = int(numStr)
+        #         j = i+1
+        #         ans.append(s[j:j+num])
+        #         i = j+num-1
+        #         numStr = ''
+        #         num = 0
+        #     i += 1
+        ans = []
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != '/':
+                j += 1
+            length = int(s[i:j])
+            ans.append(s[j+1:j+1+length])
+            i = j+1+length
+
+        return ans
