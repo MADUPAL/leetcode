9b618ee00f1e9d4d73fca68deec04e0427a45409
MADUPAL <kevin488@naver.com>
2024-07-06 18:59:19 +0900

    918

---
+++ python/918 Maximum Sum Circular Subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubarraySumCircular(self, nums: List[int]) -> int:
+        maxSum, minSum = nums[0], nums[0]
+        curMax, curMin = 0, 0
+        total = 0
+
+        for n in nums:
+            curMax = max(curMax, 0)
+            curMin = min(curMin, 0)
+            curMax += n
+            curMin += n
+            total += n
+            maxSum = max(curMax, maxSum)
+            minSum = min(curMin, minSum)
+        
+        return maxSum if maxSum < 0 else max(total-minSum, maxSum)
