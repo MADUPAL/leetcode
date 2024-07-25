2d5cad7bbae24d55918ba1c0eda4ae6e03669a31
MADUPAL <kevin488@naver.com>
2024-07-25 14:18:08 +0900

    42

---
+++ python/42 Trapping Rain Water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        L, R = 0, len(height)-1
+
+        ans = 0
+        maxL = height[0]
+        maxR = height[-1]
+        while L < R:
+            if maxL <= maxR:
+                L += 1
+                maxL = max(maxL, height[L])
+                ans += max(0, maxL - height[L])
+            else:
+                R -= 1
+                maxR = max(maxR, height[R])
+                ans += max(0, maxR - height[R])
+        
+        return ans
