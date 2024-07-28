a28f31881fcf4e5af3de80fb929aeb3dbb173e28
MADUPAL <kevin488@naver.com>
2024-07-28 23:22:55 +0900

    144

--- python/144 Binary Tree Preorder Traversal.py
+++ python/144 Binary Tree Preorder Traversal.py
@@ -6,13 +6,27 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-        def preorder(root):
-            if not root:
-                return
-            ans.append(root.val)
-            preorder(root.left)
-            preorder(root.right)
+        # ans = []
+        # def preorder(root):
+        #     if not root:
+        #         return
+        #     ans.append(root.val)
+        #     preorder(root.left)
+        #     preorder(root.right)
 
-        preorder(root)
+        # preorder(root)
+        # return ans
+
+        ans = []
+        stack = []
+        cur = root
+
+        while cur or stack:
+            if cur:
+                ans.append(cur.val)
+                stack.append(cur.right)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+        
         return ans
