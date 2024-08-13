40f5e4f42f07d62aea1e61e0b58f42b7e228ed64
MADUPAL <kevin488@naver.com>
2024-08-13 13:47:03 +0900

    42

--- python/42 Trapping Rain Water.py
+++ python/42 Trapping Rain Water.py
@@ -16,3 +16,19 @@ class Solution:
                 ans += max(0, maxR - height[R])
         
         return ans
+
+        # L, R = 0, len(height)-1
+        # maxL, maxR = height[0], height[-1]
+
+        # water = 0
+        # while L < R:
+        #     if maxL <= maxR:
+        #         L += 1
+        #         maxL = max(maxL, height[L])
+        #         water += maxL-height[L]
+        #     else:
+        #         R -= 1
+        #         maxR = max(maxR, height[R])
+        #         water += maxR-height[R]
+        
+        # return water
