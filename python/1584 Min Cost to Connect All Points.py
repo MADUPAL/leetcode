5172124a10058124194a47bbfe65b7092450766c
MADUPAL <kevin488@naver.com>
2024-09-03 22:07:04 +0900

    1584

--- python/1584 Min Cost to Connect All Points.py
+++ python/1584 Min Cost to Connect All Points.py
@@ -26,34 +26,6 @@ class UnionFind:
 
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # adjList = defaultdict(list)
-        # for src in range(len(points)):
-        #     x1, y1 = points[src]
-        #     for dst in range(src+1, len(points)):
-        #         x2, y2 = points[dst]
-        #         if src != dst:
-        #             w = abs(x1-x2)+abs(y1-y2)
-        #             adjList[src].append((dst, w))
-        #             adjList[dst].append((src, w))
-        
-        # minHeap = [(0, 0)]
-        # visited = set()
-        # ans = 0
-
-        # while len(visited) != len(points):
-        #     w, src = heapq.heappop(minHeap)
-
-        #     if src in visited:
-        #         continue
-        #     visited.add(src)
-        #     ans += w
-
-        #     for dst, nw in adjList[src]:
-        #         if dst not in visited:
-        #             heapq.heappush(minHeap, (nw, dst))
-        
-        # return ans
-
         minHeap = []
         ans = 0
         for src in range(len(points)):
@@ -72,5 +44,35 @@ class Solution:
                 continue
             ans += w
             cnt += 1
+        
+        return ans
+
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        adjList = defaultdict(list)
+        for src in range(len(points)):
+            x1, y1 = points[src]
+            for dst in range(src+1, len(points)):
+                x2, y2 = points[dst]
+                if src != dst:
+                    w = abs(x1-x2)+abs(y1-y2)
+                    adjList[src].append((dst, w))
+                    adjList[dst].append((src, w))
+        
+        minHeap = [(0, 0)]
+        visited = set()
+        ans = 0
+
+        while len(visited) != len(points):
+            w, src = heapq.heappop(minHeap)
+
+            if src in visited:
+                continue
+            visited.add(src)
+            ans += w
+
+            for dst, nw in adjList[src]:
+                if dst not in visited:
+                    heapq.heappush(minHeap, (nw, dst))
         
         return ans
