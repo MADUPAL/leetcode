74bf5238971e0716bb4a42c6989daa348bf9aba4
MADUPAL <kevin488@naver.com>
2024-09-27 14:15:35 +0900

    3

--- python/3 Longest Substring Without Repeating Characters.py
+++ python/3 Longest Substring Without Repeating Characters.py
@@ -11,3 +11,19 @@ class Solution:
             ans = max(ans, len(chars))
         
         return ans
+
+        # l, r = 0, 0
+
+        # check = set()
+        # ans = 0
+
+        # for r in range(len(s)):
+        #     while s[r] in check:
+        #         check.remove(s[l])
+        #         l += 1
+            
+        #     if s[r] not in check:
+        #         check.add(s[r])
+        #         ans = max(ans, r-l+1)
+                
+        # return ans
