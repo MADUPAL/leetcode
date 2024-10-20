6b412a39e250c9e3716d146efc499d391ff05c03
MADUPAL <kevin488@naver.com>
2024-10-20 14:43:55 +0900

    1448

--- python/1448 Count Good Nodes in Binary Tree.py
+++ python/1448 Count Good Nodes in Binary Tree.py
@@ -6,19 +6,35 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def solve(root, check):
+        # def solve(root, check):
+        #     if not root:
+        #         return 0
+            
+        #     ans = 1
+        #     for n in check:
+        #         if root.val < n:
+        #             ans = 0
+        #     check.append(root.val)
+        #     ans += solve(root.left, check)
+        #     ans += solve(root.right, check)
+        #     check.pop()
+
+        #     return ans
+        
+        # return solve(root, [])
+
+        def solve(root, maxNum):
             if not root:
                 return 0
             
-            ans = 1
-            for n in check:
-                if root.val < n:
-                    ans = 0
-            check.append(root.val)
-            ans += solve(root.left, check)
-            ans += solve(root.right, check)
-            check.pop()
+            ans = 0
+            if maxNum <= root.val:
+                maxNum = root.val
+                ans = 1
+            
+            ans += solve(root.left, maxNum)
+            ans += solve(root.right, maxNum)
 
             return ans
         
-        return solve(root, [])
+        return solve(root, root.val)
