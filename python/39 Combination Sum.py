c804973d79326cd3f0cc6bbac4b874fc57c081ca
MADUPAL <kevin488@naver.com>
2024-11-08 00:00:00 +0900

    39

--- python/39 Combination Sum.py
+++ python/39 Combination Sum.py
@@ -2,17 +2,18 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
 
-        def solve(idx, c, s):
+        def solve(idx, cur, s):
+            if s == target:
+                ans.append(cur.copy())
+                return
+            
             if idx >= len(candidates) or s > target:
                 return
-            if s == target:
-                ans.append(c.copy())
-                return
-
-            for i in range(idx, len(candidates)):
-                c.append(candidates[i])
-                solve(i, c, s + candidates[i])
-                c.pop()
+            
+            cur.append(candidates[idx])
+            solve(idx, cur, s + candidates[idx])
+            cur.pop()
+            solve(idx+1, cur, s)
         
         solve(0, [], 0)
 
